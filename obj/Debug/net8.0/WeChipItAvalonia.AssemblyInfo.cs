4ed9763609c36fc5365e04f2c33cf696ebf6653a
Jayden Wood <jaydenwood@Jaydens-MacBook-Pro.local>
2025-03-18 11:38:32 +1300

    old commit

--- obj/Debug/net8.0/WeChipItAvalonia.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeChipItAvalonia.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeChipItAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda48b51d3dad74787ff80cb88bf6b0e9c78d35a")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b8bd2db25a24d58a0f0b7caec5e03f80b40fef1")]
->>>>>>> exp1
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+666b355c765f9f6f91b6cad2033f097f0c10580e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeChipItAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeChipItAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
