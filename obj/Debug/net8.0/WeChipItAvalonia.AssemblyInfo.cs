6d38b525f3816c9b2101bd57b87ef97a39d5b5fb
JayJonah1709 <jaydenwood@Jaydens-MacBook-Pro.local>
2025-03-24 14:09:12 +1300

    modified files

--- obj/Debug/net8.0/WeChipItAvalonia.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeChipItAvalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeChipItAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ee9807763091f2e68f96a11ebc60a1af8045bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9daf18c00ff029440762fc36b89c24c7bf34f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeChipItAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeChipItAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
